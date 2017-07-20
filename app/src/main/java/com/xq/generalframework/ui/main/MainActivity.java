c003a32515a58a166d84a7c08c207ea7958377ec
SKY-20160523ZQP\admin <571129524@qq.com>
2017-07-20 19:22:41 +0800

    重写

--- app/src/main/java/com/xq/generalframework/ui/main/MainActivity.java
+++ app/src/main/java/com/xq/generalframework/ui/main/MainActivity.java
@@ -1,7 +1,8 @@
 package com.xq.generalframework.ui.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.xq.generalframework.R;
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onJump(View view) {
+
     }
 }
